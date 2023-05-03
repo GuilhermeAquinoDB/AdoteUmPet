00ea4cf9ad099ed9e7f2e1aea7515b08262ba863
Guilherme de Aquino <49727289+GuilhermeAquinoDB@users.noreply.github.com>
2023-05-03 12:19:06 -0300

    implementando delete pet

--- src/main/java/br/com/student/api/pet/controllers/PetController.java
+++ src/main/java/br/com/student/api/pet/controllers/PetController.java
@@ -1,21 +1,21 @@
 package br.com.student.api.pet.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
+import br.com.student.core.models.Pet;
+import br.com.student.core.repositories.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.student.api.pet.dtos.PetRequest;
 import br.com.student.api.pet.dtos.PetResponse;
 import br.com.student.api.pet.services.PetService;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/pets")
@@ -23,6 +23,7 @@ public class PetController {
 	
 	@Autowired
 	private PetService petService;
+	private PetRepository petRepository;
 	
 	@GetMapping
 	public List<PetResponse> findAll() {
@@ -33,7 +34,15 @@ public class PetController {
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public PetResponse create(@RequestBody @Valid PetRequest petRequest) {
+
 		return petService.create(petRequest);
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+
+		petService.excluir(id);
 	}
 	
 }
--- src/main/java/br/com/student/api/pet/services/PetService.java
+++ src/main/java/br/com/student/api/pet/services/PetService.java
@@ -1,15 +1,19 @@
 package br.com.student.api.pet.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import br.com.student.core.models.Pet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import br.com.student.api.pet.dtos.PetRequest;
 import br.com.student.api.pet.dtos.PetResponse;
 import br.com.student.api.pet.mappers.PetMapper;
 import br.com.student.core.repositories.PetRepository;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class PetService {
@@ -33,6 +37,16 @@ public class PetService {
 		var petToCreate = petMapper.toModel(petRequest);
 		var createdPet = petRepository.save(petToCreate);
 		return petMapper.toResponse(createdPet);
+	}
+
+	public void excluir(Long id) {
+
+		Optional<Pet> pet = petRepository.findById(id);
+
+		if (pet.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+		petRepository.deleteById(id);
 	}
 
 }
